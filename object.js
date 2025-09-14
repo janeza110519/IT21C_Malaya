ca4827fc11c0d734c6ee933dd46e139f3c3e9a62
Janeza Malaya <janezamalaya4@gmail.com>
2025-09-14 03:29:57 -0800

    Added object js

---
+++ object.js
@@ -0,0 +1,29 @@
+// Object
+
+ let student = new Object ({
+    name: "Janeza Malaya",
+    age: 21,
+    isEnrolled: true
+
+});
+
+// Access properties is two ways
+
+console.log(student.name + "\n" + student.age + "\n" + student.isEnrolled);
+
+// console.log(student["name"] + "\n" + student ["age"] +"\n" + student ["isEnrolled"]);
+
+
+//let employee = {
+  //  firstname : "Janeza",
+   // lastname : "Malaya",
+   // status : "taken",
+   // occupation : "student",
+   // department : "Institute for Computer Studies",
+   // fullname: function (){
+     //    return this.firstname + " " + this.lastname + " " + this.status + " " + this.occupation + " " + this.department;
+
+   // }
+// };
+
+// console.log(employee.fullname());
